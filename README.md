9cc40e16e4d8a3dcc8e86e8f2fb90dd2cc6e162e
Guido Stein <guido@Guidos-MacBook-Air.local>
2024-04-29 23:24:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# FOSS4GNA STL 2024 Website
+
+FOSS4GNA is a Open Source Geospatial Technology conference.
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install gatsby then run the npm install in the directory.
+
+```bash
+npm i  -g gatsby
+npm i
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+
+import Layout from "../components/layout";
+import { Seo } from "../components/seo";
+
+const IndexPage = () => (
+  <Layout>
+    <section className="py-5 text-center container">
+      <div className="row py-lg-5">
+        <div className="col-lg-6 col-md-8 mx-auto">
+          <StaticImage
+            src="../images/square_logo_foss4gna_stl_2024.png"
+            width={300}
+            quality={95}
+            formats={["AUTO", "WEBP"]}
+            alt="A Gatsby astronaut"
+            className="img-fluid"
+          />
+          <h1 className="fw-light">September 9 - 11</h1>
+          <p className="lead text-muted">Scaling Geospatial into the future</p>
+          <p>
+            FOSS4G North America is the premier open geospatial technology and
+            business conference with a vibrant and welcoming atmosphere for
+            technologists, end-users, academics, and decision makers. This
+            year's conference will be held at the Hyatt Regency St. Louis At The
+            Arch. The conference dates are September 9th  through 11th.
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <Link to="https://us19.list-manage.com/contact-form?u=8635900ee697ecbb8a595f92d&form_id=2a26281da80916604418c94616d3a1ac" className="btn btn-primary my-2">
+          Join the mailing list for future announcements
+        </Link>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default IndexPage;
+
+export const Head = () => <Seo />;
