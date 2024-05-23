e384ef77f3888aaec827888c5d21d4cc663b6d54
Guido Stein <guido@guidostein.com>
2024-05-23 08:25:24 -0400

    Add about page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,13 +2,13 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 
-// const isActive = ({ isCurrent }) => {
-//   return isCurrent
-//     ? { className: "nav-link active" }
-//     : { className: "nav-link" };
-// };
+const isActive = ({ isCurrent }) => {
+  return isCurrent
+    ? { className: "nav-link active" }
+    : { className: "nav-link" };
+};
 
-// const ExactNavLink = (props) => <Link getProps={isActive} {...props} />;
+const ExactNavLink = (props) => <Link getProps={isActive} {...props} />;
 
 const Navbar = ({ siteTitle }) => {
   return (
@@ -29,24 +29,16 @@ const Navbar = ({ siteTitle }) => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/*<div className="collapse navbar-collapse" id="main-navbar">
+        <div className="collapse navbar-collapse" id="main-navbar">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
-           <li className="nav-item">
-              <ExactNavLink
-                to="/"
-              >
-                Home
-              </ExactNavLink>
+            <li className="nav-item">
+              <ExactNavLink to="/">Home</ExactNavLink>
             </li>
             <li className="nav-item">
-              <ExactNavLink
-                to="/about"
-              >
-                About
-              </ExactNavLink>
+              <ExactNavLink to="/about">About</ExactNavLink>
             </li>
           </ul>
-        </div> */}
+        </div>
       </div>
     </nav>
   );
